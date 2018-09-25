96bb9fbf6a2922f61411434ed7cded2a1e560732
jtrtj <jt.r@live.com>
2018-09-25 15:26:37 -0600

    fix even listener for reset and clear buttons

--- scripts/number_guesser.js
+++ scripts/number_guesser.js
@@ -1,16 +1,20 @@
-const guess = document.getElementById("guess")
-guess.oninput = function() {enableClearButton()};
+const guessField = document.getElementById("guess");
+guessField.oninput = function() {enableButton("resetButton")
+                                 enableButton("clearButton")};
 
-const clearButton = document.getElementById("clearButton")
+const clearButton = document.getElementById("clearButton");
 clearButton.onclick = function() {clearGuessField()};
 
 const guessButton = document.getElementById("guessButton");
-guessButton.onclick = function() {checkGuess()};
+guessButton.onclick = function() {checkGuess(guess)};
+
+const resetButton = document.getElementById("resetButton");
+resetButton.onclick = function() {resetGame()};
 
 const gameOutput = document.getElementById("gameOutput");
 
-function enableClearButton() {
-  let button = document.getElementById("clearButton");
+function enableButton(id) {
+  let button = document.getElementById(id);
   button.className = "enabled-button";
 };
 
@@ -19,13 +23,18 @@ function clearGuessField() {
   clearButton.className = "disabled-button";
 };
 
+function resetGame() {
+  location.reload()
+};
+
 function randNum(start, end) {
   num = Math.floor((Math.random() * end) + start);
   return num;
 };
 
-
-const number = randNum(1, 100);
+const start = 1
+const end = 100
+const number = randNum(start, end);
 console.log(number);
 
 function overUnder(playerGuess) {
@@ -41,12 +50,14 @@ function checkGuess() {
   if ( guess === number ) {
     document.getElementById("gameOutput").innerHTML = `
       <p>Your last guess was</p>
-      <h1>${guess}</h1>
+      <h1 class="guess-text">${guess}</h1>
       <p>BOOM!</p>`
+  } else if ( guess < start || guess > end ) {
+    alert("That guess is outside the range of possible answers!")
   } else {
     document.getElementById("gameOutput").innerHTML = `
-    <p>Your last guess was</p>
-    <h1>${guess}</h1>
-    <p>${overUnder(guess)}</p>`
+    <p class="game-text">Your last guess was</p>
+    <h1 class="guess-text">${guess}</h1>
+    <p class="game-text">${overUnder(guess)}</p>`
   }
 };
